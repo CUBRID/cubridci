33e3301638b2869dd4fe6699f59205f9cac4e5f8
sjkim777 <sjkim@cubrid.com>
2021-06-22 18:22:35 +0900

    Build fail check

--- docker/ci/docker-entrypoint.sh
+++ docker/ci/docker-entrypoint.sh
@@ -34,6 +34,8 @@ function run_build ()
 
   (cd $CUBRID_SRCDIR \
     && ./build.sh -p $CUBRID $@ clean build) | tee build.log | grep -e '\[[ 0-9]\+%\]' -e ' error: ' || { tail -500 build.log; false; }
+  
+  grep "Building failed" build.log && exit 1
 }
 
 function run_dist ()
