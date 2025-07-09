11b141fc8a9fab6555b267f578f01d4f9dc3b005
tw-kang <twkang@cubrid.com>
2025-07-09 20:41:54 +0900

    add git options for faster cloning, comment out db connecting function for faster job execution

--- docker/ci/docker-entrypoint.sh
+++ docker/ci/docker-entrypoint.sh
@@ -26,7 +26,7 @@ clone_repository() {
   
   debug "clone_repository $repo $branch $url" "$LINENO"
   if [ ! -d "$WORKDIR/$repo" ]; then
-    sudo -E -u "$USER" bash -c "git clone -q --depth 1 --branch $branch $url $WORKDIR/$repo"
+    sudo -E -u "$USER" bash -c "git -c fetch.parallel=0 -c core.compression=0 clone -q --depth 1 --branch $branch --single-branch --no-tags $url $WORKDIR/$repo"
   elif [ -d "$WORKDIR/$repo" ]; then
     sudo -E -u "$USER" bash -c "cd $WORKDIR/$repo && git fetch --depth 1 origin $branch && git reset --hard origin/$branch && git clean -df"
   else
@@ -58,10 +58,10 @@ run_test() {
   set +e
   report_test $TEST_REPORT $feedback_file
   ret=$?
-  set -e
-  if [ $ret -gt 0 ]; then
-    run_manual_test_result $TEST_REPORT $BASELINE
-  fi
+  # set -e
+  # if [ $ret -gt 0 ]; then
+  #   run_manual_test_result $TEST_REPORT $BASELINE
+  # fi
 
   debug "run_test() exit $ret" "$LINENO"
   exit $ret
